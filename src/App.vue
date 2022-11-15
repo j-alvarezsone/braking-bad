e3fb6e70a1fe3d324db0471438e37fc429e26ba5
Jorge Sone <jorge.sone@dataddo.com>
2022-11-15 19:41:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <h1>Hola mundo</h1>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+  }
+</style>
