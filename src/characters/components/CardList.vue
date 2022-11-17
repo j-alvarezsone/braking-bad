c78ccb473516d09f74cd1018d4c0695142100c8a
Jorge Sone <jorge.sone@dataddo.com>
2022-11-17 21:20:24 +0100

    created CharterCard component

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
   import { useQuery } from '@tanstack/vue-query';
-  // import { useCharacters } from '../composables/useCharacters';
   import type { Character } from '../interfaces/Character';
   import breakingBadApi from '../../api/breakingBadApi';
-  // const { characters, isLoading, errorMessage, hasError } = useCharacters();
+  import CharacterCard from './CharacterCard.vue';
 
   const getCharactersSlow = async (): Promise<Character[]> => {
     const { data } = await breakingBadApi.get<Character[]>('/characters');
-    return data;
+    return data.filter((character) => ![14, 17, 39].includes(character.char_id));
   };
 
   const {
@@ -25,11 +24,27 @@
 <template>
   <h1 v-if="isLoading">Loading....</h1>
   <h1 v-if="isError">{{ error }}</h1>
-  <ul>
-    <li v-for="{ char_id, name } of characters" :key="char_id">
-      {{ name }}
-    </li>
-  </ul>
+  <div class="card_list">
+    <CharacterCard v-for="character of characters" :key="character.char_id" :character="character" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .card_list {
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    gap: 1rem;
+  }
+
+  @media (max-width: 768px) {
+    .card_list {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  @media (max-width: 480px) {
+    .card_list {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
---
+++ src/characters/components/CharacterCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  import { toRefs } from 'vue';
+  import type { Character } from '../interfaces/Character';
+
+  const props = defineProps<{
+    character: Character;
+  }>();
+
+  // const replaceByDefaultImage = (event: Event) => {
+  //   const target = event.target as HTMLImageElement;
+  //   target.src = 'https://via.placeholder.com/150';
+  // };
+
+  const { img, name } = toRefs(props.character);
+</script>
+
+<template>
+  <div class="character_card">
+    <img :src="img" alt="Breaking bad images" />
+    <h3>{{ name }}</h3>
+  </div>
+</template>
+
+<style scoped>
+  .character_card {
+    /* margin-right: 5px;
+    margin-left: 5px; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* margin-bottom: 10px; */
+  }
+
+  img {
+    width: 150px;
+    max-width: 100%;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 5px 5px 0 0;
+    box-shadow: 0 0 10px 0 rgba(255, 255, 255, 0.1);
+  }
+</style>
